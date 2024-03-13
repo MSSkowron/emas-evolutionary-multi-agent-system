e82f444bb3244cf01347506b2d0973e13845a358
Ch-Adrian <aderter116@gmail.com>
2024-03-13 23:17:20 +0100

    feat: add energy loss to agents and plots

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import statistics
+import plotille
 
 settings = {
     "parameters":{
@@ -29,6 +31,10 @@ class Agent:
         self.energy = energy
 
     def fight(self, neighbor):
+        # energy loss due to fight
+        self.energy -= self.energy * 0.05
+        neighbor.energy -= neighbor.energy * 0.05
+
         if self.genotype > neighbor.genotype:
             energy = neighbor.energy * 0.1 # Zabieramy część energii sąsiada
             self.energy += energy
@@ -39,9 +45,14 @@ class Agent:
             self.energy -= energy
 
     def reproduce(self, neighbor, reqEnergy):
+        # energy loss due to reproduction
+        self.energy -= self.energy * 0.1
+        neighbor.energy -= neighbor.energy * 0.1
+
         if self.energy > reqEnergy and neighbor.energy > reqEnergy:
             child_genotype = (self.genotype + neighbor.genotype) / 2  # Średnia genotypów rodziców
             child_energy = (self.energy + neighbor.energy) / 2  # Średnia energii rodziców
+            child_energy += child_energy * 0.2 # energy boost for newborn
             return Agent(child_genotype, child_energy)
         else:
             return None
@@ -81,28 +92,51 @@ class Island:
 
     def migrate(self, to_be_migrated):
         for agent in to_be_migrated:
-            self.agents.remove(agent)
             island = random.choice(self.islands)
             island.agents.append(agent)
+            agent.energy -= agent.energy * 0.1
+            self.agents.remove(agent)
+
         
+def prepare_input_data():
+    for _ in range(settings["parameters"]["islands"]):
+        maxStartingGenotype = random.randint(1, settings["parameters"]["maxStartingGenotype"])
+        maxStartingEnergyLvl = random.randint(1, settings["parameters"]["maxStartingEnergyLvl"])
+        yield maxStartingGenotype, maxStartingEnergyLvl, settings["parameters"]["agentsPerIsland"]
 
 def main():
-    islands=[]
-    for _ in range(settings["parameters"]["islands"]):
-        island = Island([Agent(random.randint(1, settings["parameters"]["maxStartingGenotype"]), random.randint(1, settings["parameters"]["maxStartingEnergyLvl"])) for _ in range(settings["parameters"]["agentsPerIsland"])],islands)
+    islands = []
+    output_data = { "iteration": [], "island": []}
+
+    for input_data in prepare_input_data():
+        island = Island([Agent(input_data[0], input_data[1]) for _ in range(input_data[2])], islands)
         islands.append(island)
+        output_data["island"].append([])
 
-
-    for _ in range(settings["parameters"]["iterations"]):
-        for island in islands:
+    for it in range(settings["parameters"]["iterations"]):
+        output_data["iteration"].append(it)
+        for idx, island in enumerate(islands):
             island.perform_actions()
             island.perform_death()
-
+            energies = [agent.energy for agent in island.agents]
+            if len(energies) == 0:
+                energies.append(0)
+            output_data["island"][idx].append(statistics.mean(energies))
 
     for i, island in enumerate(islands):
         print(f"Final agents on island {i}:")
         for agent in island.agents:
             print("Genotype:", agent.genotype, "Energy:", agent.energy)
+
+        fig = plotille.Figure()
+        print("\nGenotype and energy plot:")
+        genotype_Y = [agent.genotype for agent in island.agents]
+        energy_X = [agent.energy for agent in island.agents]
+        print(plotille.plot(energy_X, genotype_Y, height=10, width=60, interp="linear", lc="yellow"))
+
+        print("\nEnergy plot for iterations:")
+        print(plotille.plot(output_data["iteration"], output_data["island"][i], height=30, width=60, interp="linear", lc="cyan"))
+
 
 if __name__ == "__main__":
     main()
