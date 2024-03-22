d392e7fe0ec56ac743e1343d5a7923b87c4e5c23
MSSkowron <mateusz.skowron@poczta.onet.pl>
2024-03-22 10:38:39 +0100

    Refactor rastrigin

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ settings = {
     ]
 }
 
+
 class Agent:
     def __init__(self, x, s):
         self.energy = rastrigin(x)
@@ -50,32 +51,31 @@ class Agent:
             self.energy = -1
 
     def reproduce(self, neighbor):
+        self_s = copy.copy(self.s)
+        self_s[0] *= np.exp(np.random.normal(0, self.d))
+        self_s[1] *= np.exp(np.random.normal(0, self.d))
 
-        selfS = copy.copy(self.s) 
-        selfS[0] *= np.exp(np.random.normal(0, self.d))
-        selfS[1] *= np.exp(np.random.normal(0, self.d))
+        self_x = copy.copy(self.x)
+        self_x[0] += np.random.normal(0, self_s[0])
+        self_x[1] += np.random.normal(0, self_s[1])
 
-        selfX = copy.copy(self.x)
-        selfX[0] += np.random.normal(0, selfS[0])
-        selfX[1] += np.random.normal(0, selfS[1])
+        neighbor_s = copy.copy(neighbor.s)
+        neighbor_s[0] *= np.exp(np.random.normal(0, neighbor.d))
+        neighbor_s[1] *= np.exp(np.random.normal(0, neighbor.d))
 
-        neighborS = copy.copy(neighbor.s) 
-        neighborS[0] *= np.exp(np.random.normal(0, neighbor.d))
-        neighborS[1] *= np.exp(np.random.normal(0, neighbor.d))
-
-        neighborX = copy.copy(neighbor.x)
-        neighborX[0] += np.random.normal(0, neighborS[0])
-        neighborX[1] += np.random.normal(0, neighborS[1])
+        neighbor_x = copy.copy(neighbor.x)
+        neighbor_x[0] += np.random.normal(0, neighbor_s[0])
+        neighbor_x[1] += np.random.normal(0, neighbor_s[1])
 
         newborn = 0
         cross_genotype = 0
         if self.energy < neighbor.energy:
-            newborn = Agent(selfX, selfS)
-            cross_genotype = (neighborX, neighborS)
+            newborn = Agent(self_x, self_s)
+            cross_genotype = (neighbor_x, neighbor_s)
         else:
-            newborn = Agent(neighborX, neighborS)
-            cross_genotype = (selfX, selfS)
-        
+            newborn = Agent(neighbor_x, neighbor_s)
+            cross_genotype = (self_x, self_s)
+
         if random.randint(1, 4) == 4:
             newborn.x[0] = cross_genotype[0][0]
             newborn.s[0] = cross_genotype[1][0]
@@ -83,12 +83,12 @@ class Agent:
         if random.randint(1, 4) == 4:
             newborn.x[1] = cross_genotype[0][1]
             newborn.s[1] = cross_genotype[1][1]
-        
+
         return newborn
 
     def is_dead(self):
         return self.energy < 0
-    
+
     def print(self):
         print("e: ", self.energy)
 
@@ -105,26 +105,26 @@ class Island:
         initial_agent_amount = len(self.agents)
 
         for idx, agent in enumerate(self.agents):
-            neighbor = random.choice([ agent_t for agent_t in self.agents if agent_t != agent])
+            neighbor = random.choice([agent_t for agent_t in self.agents if agent_t != agent])
             child = agent.reproduce(neighbor)
             self.agents.append(child)
-            if idx == initial_agent_amount-1:
+            if idx == initial_agent_amount - 1:
                 break
 
         for idx, agent in enumerate(self.agents):
             if agent.is_dead():
-                initial_agent_amount+=1
+                initial_agent_amount += 1
                 continue
-            neighbor = random.choice([ agent_t for agent_t in self.agents if agent_t != agent and not agent_t.is_dead()])
+            neighbor = random.choice([agent_t for agent_t in self.agents if agent_t != agent and not agent_t.is_dead()])
             agent.fight(neighbor)
-            if idx == initial_agent_amount-1:
+            if idx == initial_agent_amount - 1:
                 break
 
         self.perform_death()
         for agent in self.agents:
             if random.randint(0, 11) == 10:
                 to_be_migrated.append(agent)
-        
+
         self.migrate(to_be_migrated)
 
     def perform_death(self):
@@ -139,11 +139,12 @@ class Island:
 
 def prepare_input_data():
     for _ in range(settings["parameters"]["islands"]):
-        x = [random.uniform(settings["parameters"]["minRast"], settings["parameters"]["maxRast"]), \
-                random.uniform(settings["parameters"]["minRast"], settings["parameters"]["maxRast"])]
-        s = [random.uniform(settings["parameters"]["minVec"], settings["parameters"]["maxVec"]), \
-                random.uniform(settings["parameters"]["minVec"], settings["parameters"]["maxVec"])]
+        x = [random.uniform(settings["parameters"]["minRast"], settings["parameters"]["maxRast"]),
+             random.uniform(settings["parameters"]["minRast"], settings["parameters"]["maxRast"])]
+        s = [random.uniform(settings["parameters"]["minVec"], settings["parameters"]["maxVec"]),
+             random.uniform(settings["parameters"]["minVec"], settings["parameters"]["maxVec"])]
         yield x, s, settings["parameters"]["agentsPerIsland"]
+
 
 def main():
     islands = []
@@ -155,9 +156,10 @@ def main():
         islands.append(island)
         output_data["island"].append([])
         scatter_data["island"].append([])
-        
+
     for island_idx in range(len(islands)):
-        scatter_data["island"][island_idx].append([(agent.x[0], agent.x[1], agent.energy) for agent in islands[island_idx].agents])
+        scatter_data["island"][island_idx].append(
+            [(agent.x[0], agent.x[1], agent.energy) for agent in islands[island_idx].agents])
 
     for it in range(settings["parameters"]["iterations"]):
         output_data["iteration"].append(it)
@@ -167,13 +169,15 @@ def main():
             if len(energies) == 0:
                 energies.append(0)
             output_data["island"][island_idx].append(statistics.mean(energies))
-            scatter_data["island"][island_idx].append([ (agent.x[0], agent.x[1], agent.energy) for agent in islands[island_idx].agents ])
+            scatter_data["island"][island_idx].append(
+                [(agent.x[0], agent.x[1], agent.energy) for agent in islands[island_idx].agents])
 
     for island_idx, island in enumerate(islands):
         print(f"Final agents on island {island_idx}:")
 
         print("\nEnergy plot for iterations:")
-        print(plotille.plot(output_data["iteration"], output_data["island"][island_idx], height=30, width=60, interp="linear",
+        print(plotille.plot(output_data["iteration"], output_data["island"][island_idx], height=30, width=60,
+                            interp="linear",
                             lc="cyan"))
 
         fig = plt.figure(figsize=(12, 12))
@@ -186,7 +190,7 @@ def main():
         rate2 = 0.02
         rate3 = 0.003
 
-        min_point = [-1,-1,-1]
+        min_point = [-1, -1, -1]
 
         for series_idx, series in enumerate(scatter_data["island"][island_idx]):
             x_val = []
@@ -204,11 +208,12 @@ def main():
                     min_point[1] = point[1]
                     min_point[2] = point[2]
 
-                ax.scatter(x_val, y_val, z_val, color=[rate1*series_idx, rate2*series_idx, rate3*series_idx])
+                ax.scatter(x_val, y_val, z_val, color=[rate1 * series_idx, rate2 * series_idx, rate3 * series_idx])
 
         print("Minimum in: x: ", min_point[0], " y: ", min_point[1], " energy: ", min_point[2])
 
         plt.show()
+
 
 if __name__ == "__main__":
     main()
--- rastrigin.py
+++ rastrigin.py
@@ -1,36 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-import random
 import matplotlib
 
-def rastrigin(X, dim = 2, A = 10):
-    if len(X) != dim: return 0
-    return A * dim + np.sum([ (x**2 - A*math.cos(2*math.pi*x)) for x in X])
-        
+
+def rastrigin(x, dim=2, a=10):
+    if len(x) != dim:
+        return 0
+    return a * dim + np.sum([(xi ** 2 - a * np.cos(2 * np.pi * xi)) for xi in x])
+
+
+def generate_points(min_val, max_val, num_points):
+    return np.linspace(min_val, max_val, num_points)
+
 
 if __name__ == "__main__":
-    
-    dimension = 2
-    amount_of_points = 100
-    X = np.linspace(-5.12, 5.12, amount_of_points)
-    Y = np.linspace(-5.12, 5.12, amount_of_points)
-    A = []
-    B = []
-    C = []
+    dimension, amount_of_points = 2, 100
+    X = generate_points(-5.12, 5.12, amount_of_points)
+    Y = generate_points(-5.12, 5.12, amount_of_points)
 
-    for x in range(amount_of_points):
-        A.append([])
-        B.append([])
-        C.append([])
-        for y in range(amount_of_points):
-            A[x].append(X[x])
-            B[x].append(Y[y])
-            result = rastrigin([X[x],Y[y]])
-            C[x].append(result)
+    A = [[X[x] for _ in range(amount_of_points)] for x in range(amount_of_points)]
+    B = [[Y[y] for y in range(amount_of_points)] for _ in range(amount_of_points)]
+    C = [[rastrigin([X[x], Y[y]]) for y in range(amount_of_points)] for x in range(amount_of_points)]
 
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-    ax.plot_surface(np.array(A), np.array(B), np.array(C), rstride=1, cstride=1,cmap=matplotlib.cm.nipy_spectral, edgecolor='none', linewidth=0.1)
+    ax.plot_surface(np.array(A), np.array(B), np.array(C), rstride=1, cstride=1, cmap=matplotlib.cm.nipy_spectral,
+                    edgecolor='none', linewidth=0.1)
     ax.set_title('surface')
     plt.show()
